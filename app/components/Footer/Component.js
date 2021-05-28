64aa37560a63bbfa0ee187a2719f421c92e617bd
Jose Pereira <josema.pereira.cih@gmail.com>
2021-05-28 14:56:02 -0500

    Bugfix: Warnings (#1)
    * feat(Footer): add dynamical year at footer
    
    * fix(Match): fix keys warning
    
    * chore(github):  add github folder

--- app/components/Footer/Component.js
+++ app/components/Footer/Component.js
@@ -3,8 +3,7 @@ import { injectIntl } from 'react-intl';
 
 const FooterIntl = () => (
   <footer className="footer">
-    <p>&copy;Josema Pereira 2020</p>
-    <p>Este proyecto fue desarrollado para &copy;DaCodes</p>
+    <p>&copy;Josema Pereira {new Date().getFullYear()}</p>
     <p>
       Las imagenes y logotipos son pertenecientes a sus respectivos equipos y
       son usados sin fines lucrativos
